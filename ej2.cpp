a10dfd5682ad8f8860c1ed73a9743163c2d05b36
María Marino <mariamarino@Marias-MacBook-Air.local>
2022-09-19 19:45:38 -0300

    ej2 andando pero no printea bien

--- ej2.cpp
+++ ej2.cpp
@@ -1,66 +1,65 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int N;
 double L, W;
 using sprinkle = pair<int, int>;
 
-
-// pickea el sprinkle mas a la derecha que cubra ultimo_ancho_cubierto;
-// repeti
 double distance(double x1, double y1, double x2, double y2){
-    // Calculating distance
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) * 1.0);
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
-double pitagoras_H_y_C(double r, double c){
-    // Pitagoras con Hipotenusa y un cateto como datos.
-    return sqrt(pow(r, 2) - pow(c, 2) * 1.0);
+    double pitagoras(double hypotenuse, double leg){
+    return sqrt(pow(hypotenuse, 2) - pow(leg, 2));
 }
 
 
-double greedy_aspersores_2(int &n, double &l, double &w, vector<sprinkle> aspersores){
-    double ultimo_ancho_cubierto = 0;
+double greedySolve(int &n, double &l, double &w, vector<sprinkle> s){
+    double maxWidth = 0;
     int res = 0;
-        while(ultimo_ancho_cubierto < l){
-            res++;
-            int aspersor_mas_derecha = n - 1;
-            bool no_cubre;
-            no_cubre = distance(ultimo_ancho_cubierto, w/2, aspersores[aspersor_mas_derecha].second,0) > aspersores[aspersor_mas_derecha].first;
-            while(no_cubre){
-                aspersor_mas_derecha--;
-                if (distance(ultimo_ancho_cubierto, w/2, aspersores[aspersor_mas_derecha].second,0) <= aspersores[aspersor_mas_derecha].first) {
-                    no_cubre = false;
-                }
+    int lastSprinkleUsed = -1;
+    while(maxWidth < l) {
+        int rightmostSprinkle = n - 1;
+        bool dry = distance(maxWidth, w / 2, s[rightmostSprinkle].first, 0) > s[rightmostSprinkle].second;
+        while(dry && rightmostSprinkle > lastSprinkleUsed){
+            rightmostSprinkle--;
+            if (distance(maxWidth, w / 2, s[rightmostSprinkle].first, 0) <= s[rightmostSprinkle].second) {
+                dry = false;
             }
-            ultimo_ancho_cubierto = pitagoras_H_y_C(aspersores[aspersor_mas_derecha].first, w/2);
         }
-    return res;
+        maxWidth = pitagoras(s[rightmostSprinkle].second, w / 2) + s[rightmostSprinkle].first;
+        lastSprinkleUsed = rightmostSprinkle;
+        res++;
+    }
+    return maxWidth >= l ? res : -1;
 }
 
 int main()
 {
     int test_case = 0;
     vector<double> res;
-    while(scanf("%d%lf%lf", &N, &L, &W) != EOF){
+    while(cin >> N >> L >> W) {
         vector<sprinkle> sprinkles(N, make_pair(0,0));
         for (int i = 0; i < N; i++) {
             int pos;
             int radius;
-            scanf("%d%d", &pos, &radius);
+            cin >> pos >> radius;
             sprinkles[i] = make_pair(pos,radius);
         }
-        res.push_back(greedy_aspersores_2(N, L, W, sprinkles));
+        // Sort sprinkles vector in ascending order by position
+        sort(sprinkles.begin(), sprinkles.end(), [](const sprinkle &left, const sprinkle &right) {
+            return left.first < right.first;
+        });
+        res.push_back(greedySolve(N, L, W, sprinkles));
         test_case++;
     }
 
     for (int i = 0; i < test_case; i++) {
-        cout << "Case " << i + 1 << ": " << res[i];
-        if (i != test_case - 1) {
-            cout << endl;
-        }
+        cout << res[i] << endl;
     }
 
     return 0;
